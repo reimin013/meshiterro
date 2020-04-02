eab6a57392b03f58bcf8964fd3af7e0a066c54d9
reimi <utatatan.013@gmail.com>
2020-04-02 17:39:56 +0900

    [Add] マイページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # get 'users/show' =>重複を避けるために削除
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
@@ -8,4 +9,6 @@ Rails.application.routes.draw do
   	resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
+  #'resource'=> idの受け渡し不要, 'resources' =>idの受け渡し必要
 end
